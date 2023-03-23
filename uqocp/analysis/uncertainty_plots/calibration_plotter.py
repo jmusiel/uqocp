f51e6bfe63e5d9b6bfe43a5db2e60881b0ea057a
jmusiel <73840022+jmusiel@users.noreply.github.com>
2023-03-23 21:54:40 +0000

    updates to calibration plotter

--- uqocp/analysis/uncertainty_plots/calibration_plotter.py
+++ uqocp/analysis/uncertainty_plots/calibration_plotter.py
@@ -9,6 +9,7 @@ import recalibration
 
 import json
 import numpy as np
+import os
 
 
 def plotter(name, predicted_y, stdev, true_y):
@@ -88,19 +89,88 @@ def plot_all_is2re_cal_recal(ooddict, caldict, extra_name):
         "is2re_de_stdev": lambda udict: np.array([i for i in udict["is2re_de_stdev"]]),
     }.items():
         plot_recal(
-            name="id_ood/"+name+extra_name,
-            cal_predicted_y= np.array(caldict["dft_de"]),
-            cal_stdev=method(caldict),
-            cal_true_y= np.array(caldict["is2re_de"]),
-            predicted_y=np.array(ooddict["dft_de"]),
-            stdev=method(ooddict),
-            true_y=np.array(ooddict["is2re_de"]),
+            name=extra_name+"/"+name,
+            cal_predicted_y= np.array(caldict["dft_de"])+ 1e-8,
+            cal_stdev=method(caldict)+ 1e-8,
+            cal_true_y= np.array(caldict["is2re_de"])+ 1e-8,
+            predicted_y=np.array(ooddict["dft_de"])+ 1e-8,
+            stdev=method(ooddict)+ 1e-8,
+            true_y=np.array(ooddict["is2re_de"])+ 1e-8,
         )
+
+def plot_all_ens_de_mean_cal_recal(ooddict, caldict, extra_name):
+    for name, method in {
+        "s2e_max": lambda udict: np.array([max(i) for i in udict["inf_e_stdev"]]),
+        "s2f_max": lambda udict: np.array([max([max(j) for j in i]) for i in udict["inf_f_stdev"]]),
+        "ens_e_max": lambda udict: np.array([max(i) for i in udict["ens_e_stdev"]]),
+        "ens_s2f_max": lambda udict: np.array([max([max(j) for j in i]) for i in udict["ens_f_stdev"]]),
+        "is2re_de_stdev": lambda udict: np.array([i for i in udict["is2re_de_stdev"]]),
+    }.items():
+        plot_recal(
+            name=extra_name+"/"+name,
+            cal_predicted_y= np.array(caldict["dft_de"])+ 1e-8,
+            cal_stdev=method(caldict)+ 1e-8,
+            cal_true_y= np.abs(np.array(caldict["ens_de_mean"]))+ 1e-8,
+            predicted_y=np.array(ooddict["dft_de"])+ 1e-8,
+            stdev=method(ooddict)+ 1e-8,
+            true_y=np.abs(np.array(ooddict["ens_de_mean"]))+ 1e-8,
+        )
+
+def flatten(l):
+    return [y for x in l for y in x]
+
+def plot_all_s2e_vs_s2f_cal_recal(ooddict, caldict, extra_name):
+
+    cal_predicted_y = np.array(flatten(caldict["inf_e_error"]))+ 1e-8
+    predicted_y = np.array(flatten(ooddict["inf_e_error"]))+ 1e-8
+    plot_recal(
+        name=extra_name+"/"+"s2e_predict_s2e",
+        cal_predicted_y= cal_predicted_y,
+        cal_stdev= np.array(flatten(caldict["inf_e_stdev"]))+ 1e-8,
+        cal_true_y= np.zeros_like(cal_predicted_y),
+        predicted_y=predicted_y,
+        stdev= np.array(flatten(ooddict["inf_e_stdev"]))+ 1e-8,
+        true_y=np.zeros_like(predicted_y),
+    )
+
+    plot_recal(
+        name=extra_name+"/"+"s2fl2max_predict_s2e",
+        cal_predicted_y= cal_predicted_y,
+        cal_stdev= np.array(flatten([[max(j) for j in i] for i in caldict["inf_f_stdev"]]))+ 1e-8,
+        cal_true_y= np.zeros_like(cal_predicted_y),
+        predicted_y=predicted_y,
+        stdev= np.array(flatten([[max(j) for j in i] for i in ooddict["inf_f_stdev"]]))+ 1e-8,
+        true_y=np.zeros_like(predicted_y),
+    )
+
+    cal_predicted_y = np.array(flatten([[max(j) for j in i] for i in caldict["inf_f_l2error"]])) + 1e-8
+    predicted_y = np.array(flatten([[max(j) for j in i] for i in ooddict["inf_f_l2error"]]))+ 1e-8
+    plot_recal(
+        name=extra_name+"/"+"s2e_predict_s2fl2max",
+        cal_predicted_y= cal_predicted_y,
+        cal_stdev= np.array(flatten(caldict["inf_e_stdev"]))+ 1e-8,
+        cal_true_y= np.zeros_like(cal_predicted_y),
+        predicted_y=predicted_y,
+        stdev= np.array(flatten(ooddict["inf_e_stdev"]))+ 1e-8,
+        true_y=np.zeros_like(predicted_y),
+    )
+
+    plot_recal(
+        name=extra_name+"/"+"s2l2fmax_predict_s2fl2max",
+        cal_predicted_y= cal_predicted_y,
+        cal_stdev= np.array(flatten([[max(j) for j in i] for i in caldict["inf_f_stdev"]]))+ 1e-8,
+        cal_true_y= np.zeros_like(cal_predicted_y),
+        predicted_y=predicted_y,
+        stdev= np.array(flatten([[max(j) for j in i] for i in ooddict["inf_f_stdev"]]))+ 1e-8,
+        true_y=np.zeros_like(predicted_y),
+    )
+
 
 
 if __name__ == "__main__":
     json_path = "/home/jovyan/shared-scratch/joe/jobs/uncertainty/uqocp/uqocp/analysis/ood_errors_traj2traj.json"
     # json_path = "/home/jovyan/shared-scratch/joe/jobs/uncertainty/uqocp/uqocp/analysis/OC22_oc22_traj2traj.json"
+    # json_path = "/home/jovyan/shared-scratch/joe/jobs/uncertainty/uqocp/uqocp/analysis/Sudheesh_mof_zeolite_errors_traj2traj.json"
     with open(json_path, "r") as jsonfile:
         ooddict = json.load(jsonfile)
     
@@ -109,4 +179,19 @@ if __name__ == "__main__":
         caldict = json.load(jsonfile)
 
 
-    plot_all_is2re_cal_recal(ooddict, caldict, "_id_ood")
+    # plot_all_is2re_cal_recal(ooddict, caldict, "id_ood_is2re")
+    # plot_all_s2e_vs_s2f_cal_recal(ooddict, caldict, "ood_s2f_vs_s2e")
+    # plot_all_s2e_vs_s2f_cal_recal(ooddict, caldict, "szeolite_s2f_vs_s2e")
+    # plot_all_is2re_cal_recal(ooddict, caldict, "szeolite_is2re")
+
+
+    for i, j in [
+        ("ens_de_mean_ood","/home/jovyan/shared-scratch/joe/jobs/uncertainty/uqocp/uqocp/analysis/ood_errors_traj2traj.json"),
+        ("ens_de_mean_oc22","/home/jovyan/shared-scratch/joe/jobs/uncertainty/uqocp/uqocp/analysis/OC22_oc22_traj2traj.json"),
+        ("ens_de_mean_szeolite","/home/jovyan/shared-scratch/joe/jobs/uncertainty/uqocp/uqocp/analysis/Sudheesh_mof_zeolite_errors_traj2traj.json"),
+    ]: 
+        os.mkdir(i)
+        json_path = j
+        with open(json_path, "r") as jsonfile:
+            ooddict = json.load(jsonfile)
+        plot_all_ens_de_mean_cal_recal(ooddict, caldict, i)
